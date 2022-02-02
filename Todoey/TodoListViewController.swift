e2ac47d85e83fb45463138d2ce6280f43839c13d
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-02-02 19:37:21 +0200

    add table datasource methods; display items on screen

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["Buy Milk", "Find socks"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    // MARK: - Table Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
     }
 }
 
