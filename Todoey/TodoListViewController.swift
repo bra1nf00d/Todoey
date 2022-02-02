3e2a43b85b5396ff04237673ad9a64dafd5153fb
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-02-02 20:05:48 +0200

    create an alert to add new items

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -40,5 +40,18 @@ class TodoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    // MARK: - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            print("added")
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
 
