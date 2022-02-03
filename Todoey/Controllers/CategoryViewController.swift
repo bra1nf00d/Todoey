163f4dd21deb4ccff4982bf1e19fae71d888cba6
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-02-03 17:51:20 +0200

    add category screen

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Vladyslav Taranenko on 03.02.2022.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
